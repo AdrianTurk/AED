80df05ba4266af5b62cd3f70d3d619bb54e8513d
Mario Adrian Turk <ma_turk@yahoo.com>
2021-04-29 23:32:10 -0300

    clase 4

---
+++ OO-CppHelloWorld/prueba.cpp
@@ -0,0 +1,10 @@
+#define NDEBUG
+#include <assert.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+    assert (2+2==5);
+    cout << "Clarooo, 2x2=5 Y las ovejas tambien vuelan :P";
+}
+
