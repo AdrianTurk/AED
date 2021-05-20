ec942d0a01d10ec3695d90f7a45c111265af91e4
Mario Adrian Turk <ma_turk@yahoo.com>
2021-05-20 15:36:37 -0300

    Last commit 20/5

--- 01-Pruebas Unitarias con Assert/01.main.cpp
+++ 01-Pruebas Unitarias con Assert/01.main.cpp
@@ -3,26 +3,15 @@
 #include <cassert>
  
 using namespace std;
+using namespace std::literals;
+//using namespace std::string_literals;
  
 int main()
 {
-    //Strings
-    assert(string("texto1") + string("texto2") == "texto1texto2");  // No debería???
-    assert(string("TextoLargo").at(3)=='t');
-    assert(string("TEXTO").length()==5);
-    
-    // strings Alternativo:
-    assert("texto1"s + "texto2"s == "texto1texto2");    // Tampoco Debería? Tiene pinta de operator overloading en OOP  <- La mas natural? es valido para toda las imples?
-    
-    assert(("texto1"s + "texto2"s).compare("texto1texto2") == 0); 
-    assert(!("texto1"s + "texto2"s).compare("texto1texto2"));  //Tricky pero moy feo y poco legible :C
 
+    assert("texto1"s + "texto2"s == "texto1texto2"s);
     
-    assert(("cadena1" "cadena2")=="cadena1cadena2");
-    assert(string("cadena1" "cadena2").compare("cadena1cadena2") == 0); // Ashco :[
-
-    assert("Cadena1"=="Cadena1");           //En alguna imple podria ser false? en c deberia!!?
-    assert("Cadena1"s=="Cadena1"s);         //Es correcto? dependo de OOP?
+    assert("Cadena1"s=="Cadena1"s); 
     
     assert("TextoLargo"s.at(3)=='t');
     assert("TEXTO"s.length()==5);
@@ -31,7 +20,6 @@ int main()
     assert('a'+1=='b');
     assert('1'+2=='3');
     assert('0'+1=='1');
-    //assert(('0'++) =='1');                  //Why Don't Work :Z
     assert('0'==48);                       //Tabla ascci std?
     assert('a'==97);
     assert(char(255)+1==0);                // Los char son de 8 bits? 
@@ -47,8 +35,9 @@ int main()
     assert(0-2==-2);
     assert(5 * -2 == -10);
 
-    //assert( (-2)++ == -1);        not working. Why? :C
-
+    assert(-2 +1  == -1);
+    
+    
     //uint
     assert(32500 - 200000U == 4294799796);
     assert(0 - 2u == 4294967294);
@@ -61,7 +50,7 @@ int main()
     //Solo extenciones de c++ de ms
     //cout<< 0i8;
 
-    assert(0u -1 == 4294967295);                    // Pero acaso no existe mas el uint de 16 :C ???
+    assert(0u - 1 == 4294967295);                    // Pero acaso no existe mas el uint de 16 :C ???
     assert(0ul - 1 == 4294967295ul );               // Ulong de 32 bits?
     assert(0ull - 1 == 18446744073709551615ul) ;    // ulonglong de 64?
 
@@ -79,6 +68,16 @@ int main()
     assert(1./3 - 0.3333 < 0.0001);
     assert(25./3 - 8.3333 < 0.0001);
     assert(11./-7 - 1.5714 < 0.0001);
-    
-    cout << "La casa esta en orden";
+    assert(24E10 - 2.4E10*10 < 0.0001);
+    assert(1E-100 - 1 + 1E-100 - 1 < 0.0001);
+    assert(1.125e-5 / 1.25e10 - 1.125e-5 / 1.25E10 < 0.0001 );
+
+    cout << "La casa esta en orden" << endl;
+/*
+    string cadena= u8"te";
+    cout << std::hex << (int)cadena[0] << endl;
+    cout << "La casa esta en orden" << endl;
+    cout << u8"A";
+    cout << std::hex << (char)u8"A"[1];
+    */
 }
